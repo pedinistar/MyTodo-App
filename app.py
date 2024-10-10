9627d7518604e0365363d0cd2a10fe6b5035aace
Jayashree <pedinijayashree03@gmail.com>
2024-10-10 18:15:00 +0530

    update

--- app.py
+++ app.py
@@ -188,9 +188,11 @@ def about():
 def contact():
     return render_template('contact.html')
 
-if __name__=="__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True,port = 8000)     
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
 
 
